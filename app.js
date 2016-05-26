c54bf0997a2e18d2369700041900b2b09bd99346
voozoo <voozoo@163.com>
2016-05-27 03:07:11 +0800

    Angular Select 章节进行中

--- app.js
+++ app.js
@@ -14,16 +14,52 @@ app.controller("smallCtrl", function($scope) {
 app.controller("myCtrl", function($scope) {
 	$scope.firstName = "LianCheng";
 	$scope.lastName = "Wang";
-	
+
 	$scope.changeNum = function() {
 		$scope.number = Math.random();
 	}
 });
 
+app.service("hexUtil", function() {
+	this.toHex = function(x) {
+		return x.toString(16);
+	};
+});
+
+app.controller("srvCtl", function($scope, $location, $http, $timeout, $interval, hexUtil) {
+	$scope.absURL = $location.absUrl();
+	$http.get("index.html").then(function(response) {
+		$scope.resHTML = response.data;
+		$scope.resStatus = response.status;
+		$scope.resText = response.statusText;
+	}, function(response){
+		$scope.resHTML = response.data;
+		$scope.resStatus = response.status;
+		$scope.resText = response.statusText;
+	});
+	$timeout(function() {
+		$scope.absURL += " Write in timeout!";
+	}, 2000);
+	$interval(function() {
+		$scope.thisTime = new Date().toLocaleTimeString();
+	}, 1000);
+	$scope.hexOut = hexUtil.toHex(255);
+});
+
+app.controller("httpCtrl", function($scope, $http){
+	$http.get("customers.json").then(function(response){
+		$scope.customers = response.data.records;
+	});
+});
+
+app.controller("optCtrl", function($scope) {
+	$scope.fooNames = ["Tom", "Cat", "Jack"];
+});
+
 app.filter("myLangFlt", function() {
 	return function(lang) {
 		var starText = "";
-		for(var i = 0; i < lang.star; i++) {
+		for (var i = 0; i < lang.star; i++) {
 			starText += "*";
 		}
 		return "LANGUAGE:" + lang.name + ", STAR:" + starText;
@@ -33,8 +69,8 @@ app.filter("myLangFlt", function() {
 app.directive("testDirective", function() {
 	console.log("in the testDirective.");
 	return {
-		restrict: "M",
-		replace: true,
+		restrict : "M",
+		replace : true,
 		template : "<span>This text is made by user directive!</span>"
 	}
 });
